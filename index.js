84dabf949ff863418adc5835e54e2b59b2f6f8d4
demitryedwards <dxyz773@gmail.com>
2023-04-12 15:34:11 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+const p = document.querySelector("#text");
+
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("The DOM has loaded");
+  p.textContent = "This is really cool!";
+});
